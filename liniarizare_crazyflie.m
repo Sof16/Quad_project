fbf8d5376ed26fb31fa1744778b3fff9b31159ee
Sof16 <35914922+Sof16@users.noreply.github.com>
2018-03-28 14:58:52 +0300

    Add files via upload

--- liniarizare_crazyflie.m
+++ liniarizare_crazyflie.m
@@ -1,7 +1,10 @@
 clc;
 clear all; 
 
-m = 0.27;
+format long;
+
+m = 0.027;
+mt = 0.033;
 g = 9.81;
 Ixx = 1.395e-05;
 Iyy = 1.436e-05;
@@ -9,10 +12,17 @@ Izz = 2.173e-05;
 Th_coef = 3.1582e-10;
 To_coef = 7.9379e-12;
 d = 39.73e-3;
-omega_e = sqrt(m*g/4/Th_coef);
+omega_e = sqrt(mt*g/4/Th_coef);
 
 Th_coefOmega_e = (omega_e-4070.3)/0.2685;
 
 xe = Simulink.BlockDiagram.getInitialState('try_nelin2');
 ue = [omega_e omega_e omega_e omega_e];
 [A,B,C,D] = linmod('try_nelin2',xe,ue);
+% 
+% Q = diag([2000; 2000; 4000; 4000; 4000; 4000; 20; 20; 10; 10; 10; 10]);
+% R = 0.00003*diag([1;1;1;1]);
+% 
+% K = lqr(A,B,Q,R);
+% 
+% A = A-B*K;
